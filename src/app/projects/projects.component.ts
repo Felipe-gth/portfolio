800927c9bfa8b927af852cae3f10ba7785402b13
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-05-12 22:20:01 -0300

    Create projects, project components and /projects route

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  template: `<h1>Meus Projetos</h1>`,
+})
+export class ProjectsComponent {}
---
+++ src/app/projects/projects.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectComponent } from './project/project.component';
+import { ProjectsComponent } from './projects.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ProjectsComponent }
+];
+
+@NgModule({
+  declarations: [ProjectsComponent, ProjectComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [ProjectComponent]
+})
+export class ProjectsModule { }
