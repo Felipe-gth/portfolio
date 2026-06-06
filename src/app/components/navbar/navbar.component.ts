a8d989d9a569ed8ee30eeb351ac848e7284d3b3f
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-06-06 15:49:19 -0300

    Fix navbar bug

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
+    template: `
+    <nav class="navbar" aria-label="Navegação principal">
+        <ul class="list">
+            <li><a routerLink="">Inicio</a></li>
+            <li><a routerLink="projects">Meus projetos</a></li>
+            <li><a routerLink="">Sobre mim</a></li>
+            <li><a routerLink="">Contato</a></li>
+        </ul>
+    </nav>`,
     styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
