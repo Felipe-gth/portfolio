e02076b23df5442be4feee15518751770078394a
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-06-07 15:24:50 -0300

    Add navbar animation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
-    template: `
-    <nav class="navbar" aria-label="Navegação principal">
+    template: ` <nav
+        class="navbar"
+        [class.hidden]="isHidden"
+        aria-label="Navegação principal"
+    >
         <ul class="list">
             <li><a routerLink="">Inicio</a></li>
             <li><a routerLink="projects">Meus projetos</a></li>
@@ -14,7 +17,36 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+    private lastScroll = 0;
+    isHidden = false;
+    private tolerance = 0;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    @HostListener('window:scroll')
+    onScroll(): void {
+        const currentScroll = window.pageYOffset;
+
+        // Evita comportamento estranho no topo da página
+        if (currentScroll <= 0) {
+            this.isHidden = false;
+            return;
+        }
+
+        const scrollDifference = currentScroll - this.lastScroll;
+
+        // Descendo
+        if (scrollDifference > this.tolerance) {
+            this.isHidden = true;
+            this.lastScroll = currentScroll;
+        }
+
+        // Subindo
+        if (scrollDifference < -this.tolerance) {
+            this.isHidden = false;
+            this.lastScroll = currentScroll;
+        }
+    }
 }
