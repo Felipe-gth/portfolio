d7a18d1857e80e06ab9ae89c80293a540b4b761c
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-06-07 16:20:38 -0300

    Create navbar service

---
+++ src/app/services/navbar.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+  isHidden$ = new BehaviorSubject<boolean>(false);
+}
