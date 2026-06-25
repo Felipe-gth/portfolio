e4d36b2bc48f2a56feca1cf44515db09dec3707f
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-06-25 17:48:28 -0300

    Finish version component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,7 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [AppComponent, NavbarComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule, IconModule],
+    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, IconModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/projects/components/version/version.component.ts
+++ src/app/projects/components/version/version.component.ts
@@ -1,16 +1,35 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Version } from '../../shared/models/version.model';
 
 @Component({
     selector: 'app-version',
     templateUrl: './version.component.html',
     styleUrls: ['./version.component.css'],
+    animations: [
+        trigger('expand', [
+            transition(':enter', [
+                style({ height: 0, opacity: 0 }),
+                animate('300ms ease', style({ height: '*', opacity: 1 })),
+            ]),
+            transition(':leave', [
+                style({ height: '*', opacity: 1 }),
+                animate('300ms ease', style({ height: 0, opacity: 0 })),
+            ]),
+        ]),
+    ],
 })
 export class VersionComponent implements OnInit {
 
     @Input() versions?: Version[];
 
+    expandedIndex: number | null = null;
+
     constructor() {}
+
+    toggle(index: number): void {
+        this.expandedIndex = this.expandedIndex === index ? null : index;
+    }
 
     ngOnInit(): void {}
 }
